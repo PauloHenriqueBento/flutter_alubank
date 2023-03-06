2b6996648bebd08dc336a52da7f7540e45e02867
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2023-03-06 07:54:47 -0300

    style: menu feito

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
@@ -6,6 +7,18 @@ class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[
+              Color.fromRGBO(103, 99, 234, 1),
+              Color.fromRGBO(155, 105, 254, 1),
+              Color.fromRGBO(195, 107, 255, 1),
+            ]
+        ),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(15)),
+      ),
       child: Padding(
         padding: const EdgeInsets.fromLTRB(16, 80, 16, 16),
         child: Row(
@@ -30,7 +43,7 @@ class Header extends StatelessWidget {
                 Text(
                   'Balanço disponível',
                   style: TextStyle(fontSize: 16),
-                )
+                ),
               ],
             ),
             const Icon(
